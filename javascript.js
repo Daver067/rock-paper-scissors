0cf7e28a2fc0e67a30562f7102ed2aac03cebf45
Dave <davediemert_88@hotmail.com>
2022-03-06 17:58:25 -0800

    game rounds now consis of 5, draws do not count as a round to avoid tieing. Everything is working correctly, im going to try to add some more functions that do a litle less... still feels like my functions are too long

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 let compChoice = 0;
-let choice = 2;
+let choice = 0;
+let yourScore = 0;
+let compScore = 0;
 
 function playerChoice() { // Converts players choice to number 0 = rock 1 = paper 2 = scissors
 let choice = prompt("Rock, Paper, or Scissors?", '');
@@ -28,16 +30,22 @@ function compRandom(){ // generates random number from 0-2 for computers choice.
         return compChoice;}
 }
 
+
 function vs(){ // comparing player choice to computer choice and deciding winner.
+for (let i = 1; i < 6; i++){
 let player = playerChoice();
 let computer = compRandom();
-console.log(player);
-console.log(computer);
 if (player == computer){
-    alert('It\'s a draw')
+    alert(`It\'s a draw, this was round ${i} out of 5 but it doesn\'t count. Score is ${yourScore} for you and ${compScore} for the computer`)
+    --i;
 }
 else if ((player == 0 && computer ==2) || (player == 1 && computer == 0) || (player == 2 && computer == 1)) {
-    alert('You won!');
+    ++yourScore;
+    alert(`You Won, this was round ${i} out of 5. Score is ${yourScore} for you and ${compScore} for the computer`);
 } 
-else alert('you lost');
+else {
+++compScore;
+alert(`You Lost, this was round ${i} out of 5. Score is ${yourScore} for you and ${compScore} for the computer` );}
+}
+console.log(`Final score was ${yourScore} for you vs ${compScore} for the computer`)
 }
