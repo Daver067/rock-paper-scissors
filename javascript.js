1ef5a0cb2cbba774df2f5d4c0033d4108e5beef4
Dave <davediemert_88@hotmail.com>
2022-03-13 19:46:12 -0700

    reworked rock paper scissors completely. changed from console input to button inputs. added scoreboard to the web page. Also added the array for being able to know what choice the computer was making. Could definitely use a makeover, but the bare bones are working well.

--- javascript.js
+++ javascript.js
@@ -3,33 +3,28 @@ let choice = 0;
 let yourScore = 0;
 let compScore = 0;
 let roundsPlayed = 0;
+const array = ["Rock", "Paper", "Scissors"]
 
+const youPicked = document.querySelector('.youPicked');
+const yourScorePara = document.getElementById('yourScore');
+const compScorePara = document.getElementById('compScore');
+const result = document.getElementById('result');
+const bottom = document.getElementById('bottom');
 
-function playerChoice() { // Converts players choice to number 0 = rock 1 = paper 2 = scissors
-let choice = prompt("Rock, Paper, or Scissors?", '');
-if ((choice === null) || (choice === '')){
-    alert('please enter a value');
-    return playerChoice();
-}
-else if (choice.toLowerCase() == 'rock'){ 
-    choice = 0
-    return choice;
-}
-else if (choice.toLowerCase() == 'paper'){
-    choice = 1;
-    return choice;
-}
-else if (choice.toLowerCase() == 'scissors'){
-    choice = 2
-    return choice;
-}
-else if (choice.toLowerCase() == 'quit'){ //Testing is taking too long. Remove this when done testing.
-    choice = 7;
-    return choice;
-}
-else alert('please check your spelling');
-    return playerChoice();
-}
+const yourScoreBoard = document.getElementById('yourScoreBoard');
+const compScoreBoard = document.getElementById('compScoreBoard');
+
+const rock = document.querySelector('.rock');
+rock.addEventListener('click', () => {vs(0)});
+
+const paper = document.querySelector('.paper');
+paper.addEventListener('click', () => {vs(1)});
+
+const scissors = document.querySelector('.scissors');
+scissors.addEventListener('click', () => {vs(2)});
+
+const erase2 = document.querySelector('.reset');
+erase2.addEventListener('click', () => {erase()});
 
 
 function compRandom(){ // generates random number from 0-2 for computers choice.
@@ -44,33 +39,46 @@ function compRandom(){ // generates random number from 0-2 for computers choice.
 
 function checkTie(){ //checks to see if both inputs were the same and lowers the round counter by 1
     if (compChoice == choice) {
-        alert(`It\'s a draw, this was round ${++roundsPlayed} out of 5 but it doesn\'t count. Score is ${yourScore} for you and ${compScore} for the computer`);
         --roundsPlayed;
-        return true;
-    }
-    else return false;
-}
-
-
-function checkWin(){ //checks for player win conditions
-    if ((choice == 0 && compChoice ==2) || (choice == 1 && compChoice == 0) || (choice == 2 && compChoice == 1)) {
-        ++yourScore;
-        alert(`You Won, this was round ${++roundsPlayed} out of 5. Score is ${yourScore} for you and ${compScore} for the computer`)
+        bottom.appendChild(result).textContent = "It was a draw";
         return;
     }
     else return;
 }
 
 
-function checkLoss(){ //checks for player loss conditions
+function checkWin(){ //checks for player win conditions and updates scoreboard
+    if ((choice == 0 && compChoice ==2) || (choice == 1 && compChoice == 0) || (choice == 2 && compChoice == 1)) {
+        ++yourScore;
+        yourScoreBoard.appendChild(yourScorePara).textContent = `${yourScore}`;
+        bottom.appendChild(result).textContent = "You Won!!";
+        return;
+    }
+    else return;
+}
+
+
+function checkLoss(){ //checks for player loss conditions and updates scoreboard
     if ((choice == 0 && compChoice ==1) || (choice== 1 && compChoice == 2) || (choice == 2 && compChoice == 0)) {
         ++compScore;
-        alert(`You Lost, this was round ${++roundsPlayed} out of 5. Score is ${yourScore} for you and ${compScore} for the computer`);
+        compScoreBoard.appendChild(compScorePara).textContent = `${compScore}`;
+        bottom.appendChild(result).textContent = "You Lost :(";
         return;
     }
 else return;
 }
 
+function endGame(){ //checks to see if the first to 5 has happened
+    if (yourScore === 5) {
+        alert("You were the first to 5 points! Congratulations!!! The score will be reset, please play again!");
+        erase();
+    }
+    else if (compScore === 5) {
+        alert("The computer was the first to 5 points! Sorry!!! The score will be reset, please play again!");
+        erase();
+    }
+    else return;
+}
 
 function erase(){ // clears scoreboard after the game has ended
     compChoice = 0;
@@ -78,18 +86,22 @@ function erase(){ // clears scoreboard after the game has ended
     yourScore = 0;
     compScore = 0;
     roundsPlayed = 0;
+    compScoreBoard.appendChild(compScorePara).textContent = `${compScore}`;
+    yourScoreBoard.appendChild(yourScorePara).textContent = `${yourScore}`;
+    bottom.appendChild(youPicked).textContent = ''
+    bottom.appendChild(result).textContent = ''
 }
 
 
-function vs(){ // comparing player choice to computer choice and deciding winner.
-    for (let rounds = 1; rounds < 6; rounds++){
-    choice = playerChoice();
+
+function vs(currentChoice){ // comparing player choice to computer choice and deciding winner.
+    choice = currentChoice;
     compChoice = compRandom();
-    checkTie() == true ? (--rounds) : (rounds = rounds);
+    checkTie();
     checkWin();
     checkLoss()
-    choice == 7 ? (rounds = 5) : (rounds = rounds) // Remove this when removing 'Quit' Option
+    bottom.appendChild(youPicked).textContent = `You picked ${array[choice]} and the computer picked ${array[compChoice]}`;
+    endGame();
     }
-    console.log(`The final score was ${yourScore} for you and ${compScore} for the computer. Thanks for playing`);
-    erase();
-    }
+    
+
